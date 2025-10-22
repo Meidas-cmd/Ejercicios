cc267fb64b31c5f65fabb54a28b21b1f51fd56b5
A705\JorgeC.G. <jorgecuartero29@gmail.com>
2025-10-22 20:55:40 +0200

    no message

--- src/main/java/org/example/calculadora_simple.java
+++ src/main/java/org/example/calculadora_simple.java
@@ -14,6 +14,7 @@ public class calculadora_simple {
          double oper2 = 0;
          double oper1 = 0;
         boolean estado = true;
+        boolean fin = true;
         //mensaje de bienvenida
         System.out.println("Bienvenido a la calculadora simple de Jorge Cuartero");
         // Estoy pidiendo el primer operador y lo estoy guardando en oper1 y controlando el error con un try catch y un bucle para volver a pedirlo
@@ -59,42 +60,47 @@ public class calculadora_simple {
             case "+":
                 //Este es para suma
                 resultado = oper1+oper2;
-                System.out.println("El resultado de " + oper1 + "+" + oper2 +" es " + resultado);
                 break;
             case "-":
                 //Este es para resta
                 resultado = oper1-oper2;
-                System.out.println("El resultado de " + oper1 + "-" + oper2 +" es " + resultado);
                 break;
             case "x":
                 //Este case es pa multiplicacion
                 resultado = oper1*oper2;
-                System.out.println("El resultado de " + oper1 + "*" + oper2 +" es " + resultado);
-
                 break;
             case "/":
                 //Aqui en division estoy haciendo un if para que ninguno de los dos operadores sea 0
                 if (oper2 != CERO && oper1 != CERO) {
                     resultado = oper1 / oper2;
-                    System.out.println("El resultado de " + oper1 + "/" + oper2 + " es " + resultado);
                 }else {
                     System.out.println("No se puede dividir entre 0 ni dividir 0");
+                    fin = false;
                 }
                 break;
             case "r":
                 //Aqui mas o menos lo mismo pero que no sirva numeros negativos tambien
                 if (  oper1 > CERO) {
                     resultado = Math.sqrt(oper1);
-                    System.out.println("La raiz cuadrada de " + oper1 + " es " + resultado);
+                    System.out.println("La raiz de " + oper1 +"es " +resultado);
+                    fin = false;
+
+
                 }else {
                     System.out.println("No se puede hacer la raiz cuadrada de 0 ni de negativos");
+                    fin = false;
                 }
                 break;
             default:
                 //Este es pa cualquier otra respuesta del usuario
                 System.out.println("No podemos realizar esa operacion");
+                fin = false;
                 break;
         }
+        if (fin) {
+            System.out.println("El resultado de " + oper1 + signo + oper2 + " es " + resultado);
+        }
+
 
 
 
