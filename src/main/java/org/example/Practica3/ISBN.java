ef47d300393be0a5126dd4d0c44df04abcd0f8ce
A705\JorgeC.G. <jorgecuartero29@gmail.com>
2025-10-27 20:56:21 +0100

    no message

--- src/main/java/org/example/Practica3/ISBN.java
+++ src/main/java/org/example/Practica3/ISBN.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class ISBN {
     static void main() {
         Scanner entrada = new Scanner(System.in);
+        //Aqui he creado las variables que usaremos luego
         int posicion = 0;
         char num1;
         int num2;
@@ -13,8 +14,8 @@ public class ISBN {
         int j = 1;
         int k =0;
         int tamanyo = 0;
-        boolean estado = false;
         String isbn = "0";
+        //Aqui estoy haciendo un menu para mostrar en pantalla y coger el modo que quiere
         System.out.println("-----------------");
         System.out.println("*Validador y reparador de ISBN*");
         System.out.println("-----------------");
@@ -23,15 +24,18 @@ public class ISBN {
         System.out.println("X-Salir");
         System.out.println("-----------------");
         String modo = entrada.next().toLowerCase();
+        //Aqui estoy haciendo un if para preguntar el ISBN si elige el modo donde se utilice el ISBN
         if (modo.equals("1") || modo.equals("2")) {
             System.out.println("Introduce un ISBN");
             isbn = entrada.next().toLowerCase();
             tamanyo = isbn.length();
-            estado = true;
         }
+        //Este if es para comprobar el tamaño del isbn
         if (tamanyo == 10 || modo.equals("x")) {
             switch (modo) {
+                //En el caso que pongan 1 hacemos el validador
                 case "1":
+                    //He usado un for para hacer un bucle y vaya pasando por todo el isbn y haga las operaciones matematicas necesarias
                     for (int i = tamanyo - 1; i >= 0; i--) {
                         num1 = isbn.charAt(i);
                         if (num1 == 'x') {
@@ -42,6 +46,7 @@ public class ISBN {
                         num3 += num2 * j;
                         j = j + 1;
                     }
+                    //si todo suama un numero divisible de 11 le pone un mensaje de que es valido sino le pone un mensaje de isbn no valido
                     if (num3 % 11 == 0) {
                         System.out.println("ISBN valido");
                     } else {
@@ -49,6 +54,7 @@ public class ISBN {
                     }
                     break;
                 case "2":
+                    //Aqui hacemos el reparador haremos lo mismo que en el validador solo que en el momento que haya un ? guardamos la `posicion
                     for (int i = tamanyo - 1; i >= 0; i--) {
                         num1 = isbn.charAt(i);
                         if (num1 == 'x') {
@@ -62,6 +68,7 @@ public class ISBN {
                         num3 += num2 * j;
                         j = j + 1;
                     }
+                    //Aqui empezamos a comprobar si con 0 ya es divisible sino del 1 al 10 vamos probando hasta que sea divisible
                     if (num3 % 11 == 0) {
                         System.out.println("El numero que falta es el 0");
                     } else {
@@ -69,6 +76,7 @@ public class ISBN {
                             int sumaTest = num3 + k * posicion;
                             if (sumaTest % 11 == 0) {
                                 if (k == 10) {
+                                    System.out.println("El numero que falta es X");
                                 } else
                                     System.out.println("El número que falta es " + k);
                                 break;
@@ -77,9 +85,11 @@ public class ISBN {
                     }
                     break;
                 case "x":
+                    //Si eligen esta opcion ponemos un mensaje de saliendo y se acaba el programa
                     System.out.println("Saliendo..");
             }
         }else {
+            //Este es el mensaje si alguno de las respuesta no cumple con los requisitos.
             System.out.println(" ISBN incorrecto");
         }
     }
