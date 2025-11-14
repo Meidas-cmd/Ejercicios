77434f6c14bf7b2354eaa356156715743782a787
A705\JorgeC.G. <jorgecuartero29@gmail.com>
2025-11-14 16:30:50 +0100

    no message

--- src/main/java/org/example/Elimianr_duplicado.java
+++ src/main/java/org/example/Elimianr_duplicado.java
@@ -6,7 +6,34 @@ import java.util.Scanner;
 public class Elimianr_duplicado {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        int vec[] = {3,1,10,3,10,1,4};
 
+        int contador = 0;
+        int[] vector = {1, 2, 2, 3, 4, 4, 5};
+        int tam=0;
+        int auxiliar[]=new int [vector.length];
+        for (int i = 0; i <= vector.length-1; i++) {
+            if (i == vector.length - 1) {
+                auxiliar[i] = vector[i];
+                tam++;
+            }else if (vector[i] != vector[i + 1]) {
+                auxiliar[i] = vector[i];
+                tam++;
+            }
+        }
+        System.out.println(Arrays.toString(auxiliar));
+        System.out.println(tam);
+
+
+        int limpio[]=new int[tam];
+        int j=0;
+        for (int i = 0; i < auxiliar.length; i++) {
+            if (auxiliar[i]!=0) {
+                limpio[i]= auxiliar[i];
+                i++;
+
+            }
+        }
+
+        System.out.println(Arrays.toString(limpio));
     }
 }
--- src/main/java/org/example/Modo2_Eliminar.java
+++ src/main/java/org/example/Modo2_Eliminar.java
@@ -1,4 +1,24 @@
 package org.example;
+import java.util.Arrays;
 
 public class Modo2_Eliminar {
+    static void main() {
+        int tamanyo=0;
+        int j=0;
+        int orginal[]={1,2,2,3,4,4,5,6};
+        for (int i = 0; i < orginal.length; i++) {
+            if (orginal[i]!=orginal[i+1]){
+                orginal[i]=-1000;
+            }
+        }
+        System.out.println(Arrays.toString(orginal));
+        int limpio[]=new int[tamanyo];
+        for (int i = 0; i < orginal.length; i++) {
+            if (orginal[i]!=-1000){
+                limpio[j]=orginal[i];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(limpio));
+    }
 }
